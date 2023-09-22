534f9464257a540525fe10748bf8a47aec9d76ec
Aniket Shinde <104122533+anik36@users.noreply.github.com>
2023-09-22 16:44:58 +0530

    Update appProperties.go

--- advanced-go/application-properties/appProperties.go
+++ advanced-go/application-properties/appProperties.go
@@ -2,6 +2,8 @@ package applicationproperties
 
 const (
 	PORT_NUMBER = ":9094"
-	SERVER_CERT = "/media/aniket/9c8cbcfa-7a63-4b30-910a-2586f19beb53/Aniket/Documents/GIT ANIKET/advanced-go/SSL/server.crt"
-	SERVER_KEY  = "/media/aniket/9c8cbcfa-7a63-4b30-910a-2586f19beb53/Aniket/Documents/GIT ANIKET/advanced-go/SSL/server.key"
+	// SERVER_CERT = "/media/aniket/9c8cbcfa-7a63-4b30-910a-2586f19beb53/Aniket/Documents/GIT ANIKET/advanced-go/SSL/server.crt"
+	// SERVER_KEY  = "/media/aniket/9c8cbcfa-7a63-4b30-910a-2586f19beb53/Aniket/Documents/GIT ANIKET/advanced-go/SSL/server.key"
+	SERVER_CERT = "https://github.com/anik36/Golang/blob/advanced-go/advanced-go/SSL/server.crt"
+	SERVER_KEY = "https://github.com/anik36/Golang/blob/advanced-go/advanced-go/SSL/server.key"
 )
