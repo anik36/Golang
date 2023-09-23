54db67c02abbdbf3ad34ec05be7c0d57562c391e
anikets <aniket.shinde@merce.co>
2023-09-23 23:45:40 +0530

    used actual file path

--- advanced-go/constants/constants.go
+++ advanced-go/constants/constants.go
@@ -3,6 +3,7 @@ package constants
 // Note: "File Relative Path" is not working, hence use "File Path"
 const (
 	PORT_NUMBER = ":9094"
+	// Using actual file path
 	SERVER_CERT = "/media/aniket/9c8cbcfa-7a63-4b30-910a-2586f19beb53/Aniket/Documents/GIT ANIKET/advanced-go/ssl/server.crt"
 	SERVER_KEY  = "/media/aniket/9c8cbcfa-7a63-4b30-910a-2586f19beb53/Aniket/Documents/GIT ANIKET/advanced-go/ssl/server.key"
 	// SERVER_CERT = "./ssl/server.crt"  // this is not working (unable to locate the file)
