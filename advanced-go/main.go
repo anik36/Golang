1debfc866d44557fbbba66b77513ef32a8d44410
anikets <aniket.shinde@merce.co>
2023-09-24 11:44:31 +0530

    refactoring the code to use packages

--- advanced-go/main.go
+++ advanced-go/main.go
@@ -5,13 +5,14 @@ import (
 	"net/http"
 
 	"github.com/anik36/Golang/tree/advanced-go/constants"
+	"github.com/anik36/Golang/tree/advanced-go/handlers"
 )
 
 func main() {
 
-	http.HandleFunc("/home", Home)
-	http.HandleFunc("/", Home)
-	http.HandleFunc("/about", About)
+	http.HandleFunc("/home", handlers.Home)
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
 
 	fmt.Println("Starting application on port", constants.PORT_NUMBER)
 	_ = http.ListenAndServeTLS(constants.PORT_NUMBER, constants.SERVER_CERT, constants.SERVER_KEY, nil)
